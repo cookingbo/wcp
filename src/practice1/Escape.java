e3b3e2dbf283de33e5406b5dfc8406edd0157e3a
cookingbo <102849653+cookingbo@users.noreply.github.com>
2022-07-20 11:36:07 +0900

    [update]引数

--- src/practice1/Escape.java
+++ src/practice1/Escape.java
@@ -2,14 +2,12 @@ package practice1;
 
 public class Escape {
 	public static void main(String[] args) {
-		System.out.println("メソッドを呼び出します");
-		hello("湊");
-		hello("朝香");
-		hello("菅原");
-		System.out.println("メソッドの呼び出しが終わりました");
+		int ans = add(100, 10);
+		System.out.println("100 + 10 =" + ans);
 	}
-	public static void hello(String name) {
-		System.out.println(name + "さん、こんにちは");
+	public static int add(int x, int y) {
+		int ans = x + y;
+		return ans;
 	}
 
 }
