77b35b674689951fc5f359aee03deeacba3e4062
cookingbo <102849653+cookingbo@users.noreply.github.com>
2022-07-21 22:13:25 +0900

    [update]5章

--- src/practice1/Escape.java
+++ src/practice1/Escape.java
@@ -2,13 +2,18 @@ package practice1;
 
 public class Escape {
 	public static void main(String[] args) {
-		int ans = add(100, 10);
-		System.out.println("100 + 10 =" + ans);
+		introduceOneself();
 	}
-	public static int add(int x, int y) {
-		int ans = x + y;
-		return ans;
+	
+	public static void introduceOneself() {
+		String name = "Minato";
+		int age = 22;
+		double height = 169.9;
+		char zodiac ='辰';
+		System.out.println("私の名前は" + name + "です");
+		System.out.println("歳は" + age + "です");
+		System.out.println("身長は" + height + "cmです");
+		System.out.println("十二支は" + zodiac + "です");
 	}
 
 }
- 
