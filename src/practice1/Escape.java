64a20bfc86f17f1c67b8dd8e0fc98dafa16ad744
cookingbo <102849653+cookingbo@users.noreply.github.com>
2022-07-23 22:02:44 +0900

    [update]復習

--- src/practice1/Escape.java
+++ src/practice1/Escape.java
@@ -2,21 +2,10 @@ package practice1;
 
 public class Escape {
 	public static void main(String[] args) {
-		double triangleArea = calcSquareArea(10.0, 5.0);
-		System.out.println("正方形の面積：" + triangleArea + "㎠");
-		
-		double circleArea = calcCircleArea(5.0);
-		System.out.println("円の面積：" + circleArea + "㎠");
-	}
-	
-	public static double calcSquareArea(double bottom, double height) {
-		double area = (bottom * height);
-		return area;
-	}
-	
-	public static double calcCircleArea(double radius) {
-		double area = radius * radius * 3.14;
-		return area;
+		int x = 5;
+		int y = 10;
+		String ans = "x+yは" + (x + y);
+		System.out.println(ans);
 	}
 
 }
