833068edfa7c4d56ee80cb9c34dc3ea00928b0eb
cookingbo <102849653+cookingbo@users.noreply.github.com>
2022-07-24 16:44:36 +0900

    [update]配列練習問題

--- src/practice1/Escape.java
+++ src/practice1/Escape.java
@@ -2,14 +2,14 @@ package practice1;
 
 public class Escape {
 	public static void main(String[] args) {
-		int[] scores = {20, 30, 40, 50, 80};
-		int count = 0;
-		for (int i = 0; i < scores.length; i++) {
-			if (scores[i] <= 50) {
-				count++;
+		int[] numbers = {3, 4, 9};
+		System.out.println("1桁の数字を入力してください");
+		int input = new java.util.Scanner(System.in).nextInt();
+		for (int value : numbers) {
+			if (value == input) {
+				System.out.println("アタリ！");
 			}
 		}
-		System.out.println("50点以上の科目の数は" + count);
 	}
 
 }
