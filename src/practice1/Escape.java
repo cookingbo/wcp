97ed224f3be205d067016f8473afa00b1d337ec5
cookingbo <102849653+cookingbo@users.noreply.github.com>
2022-07-26 23:46:25 +0900

    [update]

--- src/practice1/Escape.java
+++ src/practice1/Escape.java
@@ -2,12 +2,19 @@ package practice1;
 
 public class Escape {
 	public static void main(String[] args) {
-		if (x >= 60 && y >= 60) {
-		if ((x + y) >= 130) {
-		if ((x + y) >= 100 && x >= 90 || y >= 90) {
-			
-		}
-		}
+		System.out.println("お好きなすしのネタの番号を入力してください");
+		System.out.println("1：まぐろ　2：えび　3：こはだ");
+		int sushi = new java.util.Scanner(System.in).nextInt();
+		switch (sushi) {
+		case 1:
+			System.out.println("吉");
+			break;
+		case 2:
+			System.out.println("大吉");
+			break;
+		case 3:
+			System.out.println("小吉");
+			break;
 		}
 	}
 
