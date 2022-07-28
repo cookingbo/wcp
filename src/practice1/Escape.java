0b0250887d3c3a7a1a04ff9fd7a3e42f740515fa
cookingbo <102849653+cookingbo@users.noreply.github.com>
2022-07-28 23:12:42 +0900

    [update]

--- src/practice1/Escape.java
+++ src/practice1/Escape.java
@@ -3,7 +3,7 @@ package practice1;
 public class Escape {
 	public static void main(String[] args) {
 		System.out.println("お好きなすしのネタの番号を入力してください");
-		System.out.println("1：まぐろ　2：えび　3：こはだ");
+		System.out.println("1：はまち　2：サーモン　3：大トロ");
 		int sushi = new java.util.Scanner(System.in).nextInt();
 		switch (sushi) {
 		case 1:
