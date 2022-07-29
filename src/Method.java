663507014e21503778a7ddcab3fa523dd2ff58f7
cookingbo <102849653+cookingbo@users.noreply.github.com>
2022-07-29 17:16:16 +0900

    [update]メソッド練習

---
+++ src/Method.java
@@ -0,0 +1,15 @@
+
+public class Method {
+	public static void main(String[] args) {
+		System.out.println("メソッドを呼び出します");
+		hello("湊");
+		hello("畑");
+		hello("名倉");
+		System.out.println("メソッドの呼び出しが終わりました");
+	}
+	
+	public static void hello(String name) {
+		System.out.println(name + "さんこんにちは");
+	}
+
+}
