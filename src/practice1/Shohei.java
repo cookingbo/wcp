086c05e068ef8d85375701028881204d43ca32dc
cookingbo <102849653+cookingbo@users.noreply.github.com>
2022-08-01 17:15:59 +0900

    [update]練習問題

--- src/practice1/Shohei.java
+++ src/practice1/Shohei.java
@@ -3,18 +3,12 @@ package practice1;
 public class Shohei {
 
 	public static void main(String[] args) {
-		
-		
-		int number[] = new int[20];
-		for (int i = 1; i < number.length; i++) {
-			int number2 = number[i - 1] + 5;
+		int sum = 0;
+		for (int i = 1; i <= 10; i++) {
+			sum += i;
 		}
 		
-		for (int i = 0; i < number.length; i++) {
-		      System.out.println(String.format("%2d %4d", i, number[i]));
-		    }
-		
-
+		System.out.println(sum / 10);
 	}
 
 }
