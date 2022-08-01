83686a513af6cb891b719b2f43469830524ee0d0
cookingbo <102849653+cookingbo@users.noreply.github.com>
2022-08-01 11:31:35 +0900

    [update]練習問題

--- src/practice1/Shohei.java
+++ src/practice1/Shohei.java
@@ -4,6 +4,16 @@ public class Shohei {
 
 	public static void main(String[] args) {
 		
+		
+		int number[] = new int[20];
+		for (int i = 1; i < number.length; i++) {
+			int number2 = number[i - 1] + 5;
+		}
+		
+		for (int i = 0; i < number.length; i++) {
+		      System.out.println(String.format("%2d %4d", i, number[i]));
+		    }
+		
 
 	}
 
