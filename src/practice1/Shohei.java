a90e86e26c030c4388f11ef0d0eebe7e5f3b65f1
cookingbo <102849653+cookingbo@users.noreply.github.com>
2022-08-02 23:07:15 +0900

    [update]

--- src/practice1/Shohei.java
+++ src/practice1/Shohei.java
@@ -3,12 +3,14 @@ package practice1;
 public class Shohei {
 
 	public static void main(String[] args) {
-		int sum = 0;
 		for (int i = 1; i <= 10; i++) {
-			sum += i;
+			if (i != 10) {
+				System.out.print(i + ",");
+			} else {
+				System.out.print(i);
+			}
+				
 		}
-		
-		System.out.println(sum / 10);
 	}
 
 }
