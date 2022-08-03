fb38884ad2827ead55f07514adf9c4e3a311541b
cookingbo <102849653+cookingbo@users.noreply.github.com>
2022-08-03 10:15:14 +0900

    [update]練習問題

--- src/practice1/Shohei.java
+++ src/practice1/Shohei.java
@@ -3,11 +3,18 @@ package practice1;
 public class Shohei {
 
 	public static void main(String[] args) {
-		System.out.println(number(3, 5));
+		System.out.print("数あてゲーム");
+		int ans = new java.util.Random().nextInt(10);
+		for (int i = 0; i < 5; i++) {
+			System.out.println("0～9の数字を入力してください");
+			int num = new java.util.Scanner(System.in).nextInt();
+			if (num == ans) {
+				System.out.println("アタリ！");
+			} else
+				System.out.println("違います");
+		}
+		System.out.println("ゲームを終了します");
+		
 	}
 	
-	public static int number(int sec, int thd) {
-		return (sec + thd) / 2;
-	}
-
 }
