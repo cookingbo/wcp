7bcced49d14a7f25fcc90de255023ca8d99a7b11
cookingbo <102849653+cookingbo@users.noreply.github.com>
2022-08-07 18:43:03 +0900

    [update]ループ文練習

--- src/Null01.java
+++ src/Null01.java
@@ -1,13 +1,13 @@
 
 public class Null01 {
 	public static void main(String[] args) {
-		String string1;
-		string1 = "あいう";
-		System.out.println(string1 + "の文字数：" + string1.length());
-		string1 = "";
-		System.out.println(string1 + "の文字数：" + string1.length());
-		string1 = null;
-		System.out.println(string1 + "の文字数：" + string1.length());
+		int i = 1;
+		int j = 10;
+		while (i < j) {
+			System.out.println("LOOP");
+			i *= 2;
+			j /= 2;
+		}
 	}
 
 }
