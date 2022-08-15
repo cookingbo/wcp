5214d050e595360aae688bc41478e835142a9c70
cookingbo <102849653+cookingbo@users.noreply.github.com>
2022-08-15 12:00:47 +0900

    [update]クレリック練習問題

--- src/game2/Cleric.java
+++ src/game2/Cleric.java
@@ -8,20 +8,19 @@ public class Cleric {
 	final int MAX_HP = 50;
 	int mp = 10;
 	final int MAX_MP = 10;
-
+	
 	public void selfAid() {
 		System.out.println(this.name + "はセルフエイドを唱えた！");
-		this.mp -= 5;
 		this.hp = this.MAX_HP;
-		System.out.println(this.name + "はHPが最大まで回復した");
+		this.mp -= 5;
+		System.out.println("HPが最大まで回復した！");
 	}
-
+	
 	public int pray(int sec) {
-		System.out.println(this.name + "は" + sec + "秒祈った！");
+		System.out.println(this.name + "は" + sec + "秒間天に祈った”");
 		int recover = new Random().nextInt(3) + sec;
-
 		int recoverActual = Math.min(this.MAX_MP - this.mp, recover);
-
+		
 		this.mp += recoverActual;
 		System.out.println("MPが" + recoverActual + "回復した");
 		return recoverActual;
---
+++ src/game2/ClericMain.java
@@ -0,0 +1,12 @@
+package game2;
+
+public class ClericMain {
+
+	public static void main(String[] args) {
+		Cleric c = new Cleric();
+		c.name = "Shohei";
+		c.selfAid();
+		c.pray(10);
+	}
+
+}
