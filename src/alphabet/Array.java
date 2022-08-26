96652e5139518db1e20842d0358efe675356f81c
cookingbo <102849653+cookingbo@users.noreply.github.com>
2022-08-26 21:54:50 +0900

    [update]継承練習問題

--- src/alphabet/Array.java
+++ src/alphabet/Array.java
@@ -3,7 +3,7 @@ package alphabet;
 public class Array {
 	public static void main(String[] args) {
 		int[] array = {1, 2, 3, 4};
-		System.out.println(array[0]);
+		System.out.println(array[1]);
 	}
 
 }
