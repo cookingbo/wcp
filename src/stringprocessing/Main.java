0d818754a0d393dc44b194026ae8da9ac211038c
cookingbo <102849653+cookingbo@users.noreply.github.com>
2022-09-21 22:30:02 +0900

    [update]Java API

--- src/stringprocessing/Main.java
+++ src/stringprocessing/Main.java
@@ -1,19 +1,15 @@
 package stringprocessing;
 
+import java.util.Date;
+
 public class Main {
 
 	public static void main(String[] args) {
-		String s1 = "Java and JavaScript";
-		if (s1.contains("Java")) {
-			System.out.println("文字列s1は、Javaを含んでいます");
-		}
-		
-		if (s1.endsWith("Java")) {
-			System.out.println("文字列s1は、Javaの末尾にあります");
-		}
-		
-		System.out.println("文字列s1で最初にJavaが登場する位置は" + s1.indexOf("Java"));
-		System.out.println("文字列s1で最後にJavaが登場する位置は" + s1.lastIndexOf("Java"));
+		Date now = new Date();
+		System.out.println(now);
+		System.out.println(now.getTime());
+		Date past = new Date(1600705425827L);
+		System.out.println(past);
 
 	}
 
